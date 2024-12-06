1e563440e05cbac7f8cc07f331a3f3ba3d166a16
Giovanni Peirone <134148500+GiovanniPeirone@users.noreply.github.com>
2024-12-06 19:44:57 -0300

    asd

---
+++ index.js
@@ -0,0 +1,16 @@
+var InitDemo = function() {
+   console.log('working') 
+
+    var canvas = document.getElementById('canvas')
+    var gl = canvas.getContext('webgl')
+
+
+    if(!gl){
+        console.log('webgl dont support')
+        gl = canvas.getContext('expreimental-webgl')
+    }
+
+    if(!gl){
+        alert('dont work')
+    }
+}
